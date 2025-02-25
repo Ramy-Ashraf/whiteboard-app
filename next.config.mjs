cacee3a024343c59a397feeb77b6ec6c52207b40
Ramy-Ashraf <ramy.abdelmaseeh01@eng-st.cu.edu.eg>
2025-02-25 03:08:28 +0200

    Refactor Next.js configuration to use .mjs format and maintain asset handling settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-};
-
-export default nextConfig;
+    output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/whiteboard-app2' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/whiteboard-app2/' : '',
+    images: {
+      unoptimized: true,
+    },
+  }
+  
+  module.exports = nextConfig
